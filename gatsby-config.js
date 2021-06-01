be50d970bb529e99d9dfd67d9bef34a3ff457b20
Daniel Giordano <danielgiordano@Daniels-MacBook-Pro.local>
2021-06-01 14:55:56 -0400

    Added Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId:'UA-93349937-5',
       },
     },
     `gatsby-plugin-sitemap`,
